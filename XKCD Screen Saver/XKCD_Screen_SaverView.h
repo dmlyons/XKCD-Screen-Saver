ba9daec082a351253237893962081b08d97df003
David Lyons <david@meshify.com>
2019-09-12 14:02:54 -0500

    Initial Commit

---
+++ XKCD Screen Saver/XKCD_Screen_SaverView.h
@@ -0,0 +1,13 @@
+//
+//  XKCD_Screen_SaverView.h
+//  XKCD Screen Saver
+//
+//  Created by DAVID LYONS on 9/12/19.
+//  Copyright © 2019 DAVID LYONS. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface XKCD_Screen_SaverView : ScreenSaverView
+
+@end
